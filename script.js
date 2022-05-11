e5cc74bfb2bf0c82a4c8c631e6b09eaf0b6f69a8
CarolineManalo <civymanalo@gmail.com>
2022-05-11 16:48:37 -0400

    complete task addition functionality

--- script.js
+++ script.js
@@ -1,3 +1,82 @@
+
+//first, we query the dom by targeting the form using .querySelector 
+//then, we put it into a variable called 'formElement'. this variable will be useful for the event listener.
+const formElement = document.querySelector("form");
+
+//next step is to attach the variable to an event listener.
+// after that, pass the "submit" argument as a string.
+//next, you can do an arrow function or anonymous function as the second argument. this second argument is called the callback function.
+
+// FIRST EVENT LISTENER: MAKING THE FORM LISTEN FOR A SUBMISSION TASK
+formElement.addEventListener("submit", function(event) {
+    //stop the page from refreshing when the form is submitted with preventDefault()
+    event.preventDefault();
+    console.log(event);
+
+    
+    //query the DOM for the input element and check whether it's empty
+
+    const inputElement = document.getElementById("toDoItem");
+    //when you console log inputElement, you can open up the input object and see that there's a value property. we are going to use this to target whether the user has entered text.
+    console.log(inputElement);
+
+
+    //only if the user has entered an actual task (aka input is not empty): if/else statement
+
+    if (inputElement.value !== "") {
+
+
+        //grab user's TO DO input from the form
+        console.log(inputElement.value);
+
+        // we are now going to create a new <li> element with .createElement
+        const liElement = document.createElement("li");
+
+        //display TO DO input to the page within an li element
+        //include a checkbox icon within the li
+        liElement.innerHTML = '<i class="fa-regular fa-square"></i>';
+
+        //create an element that represents the text we have to add (our task)
+        const toDoContent = document.createTextNode(inputElement.value);
+        console.log(toDoContent);
+        // and then append the text element to the li
+        liElement.appendChild(toDoContent);
+        
+
+        // add the li element to the ul
+        //so we are querying the dom for the ul, and then we are adding the li element to the ul on the page with .appendChild().
+        document.querySelector("ul").appendChild(liElement);
+        
+
+        // clear the input
+        //so we get the inputElement value and reassign it to an empty string at the end.
+        inputElement.value = "";
+
+    } else {
+        alert("Please enter a task!")
+    }
+
+    // other options from jielun 
+            //     if (inputElement.value) {
+            //         //run code here
+            //     }
+            // using truthy value
+});
+
+// SECOND EVENT LISTENER: CLICKING A TASK AS COMPLETE
+// clicking on a task allows you to toggle between unchecked/checked (aka done vs not done).
+// ******* NOTE: this will not work because you can only add event listeners to elements that exist in the DOM at the time of code execution.
+// const listElements = document.querySelector("li");
+
+// listElements.addEventListener("click", function(){
+//     console.log("To Do has been checked")
+// });
+// NOTE: ^^^ THE CODE ABOVE WILL NOT WORK, YOULL RECEIVE AN ERROR BECAUSE THE LI ELEMENT DOESN'T EXIST WHEN THE CODE EXECUTES
+
+//NEW PLAN: in order to attach a click event listener to the li's which do not exist on the page yet, we can use EVENT PROPAGATION TO DELEGATE the click event to the ul!
+// when something happens to the child element, the parents know about it. think about it like gossip in a nosy family.
+
+// PSEUDO CODE FOR WHAT WE WANT TO DO *_*_*_*_*_*_*_*_*_*_*_
 //add a submit event listener on the form
 
     //stop the page from refreshing when the form is submitted.
